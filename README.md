a65ed58486223ef1ca3448512f237e09c79866f9
MillenaOliveiraSouza <millena10.oliveira@gmail.com>
2024-08-27 23:51:19 -0300

    Primiero challenge

---
+++ README.md
@@ -0,0 +1,28 @@
+# Codificador e Decodificador de Texto
+
+Este é um projeto de codificador e decodificador de texto desenvolvido com JavaScript, CSS e HTML. O projeto foi criado para o **Alura Challenges 2024**.
+
+## Descrição
+
+O projeto permite que os usuários criptografem e descriptografem mensagens usando um conjunto específico de regras de substituição. Além disso, oferece a funcionalidade de copiar o texto resultante para a área de transferência.
+
+## Funcionalidades
+
+- **Criptografia**: Converte o texto inserido em uma forma codificada usando regras de substituição.
+- **Descriptografia**: Converte o texto codificado de volta para sua forma original.
+- **Copiar para área de transferência**: Permite que o usuário copie o texto resultante com um clique de botão.
+
+## Tecnologias Utilizadas
+
+- **JavaScript**: Para a lógica de codificação e decodificação.
+- **CSS**: Para o estilo e layout da aplicação.
+- **HTML**: Para a estrutura da página.
+
+## Como Usar
+
+1. **Digite seu texto** na área de entrada.
+2. **Clique no botão "Criptografar"** para codificar o texto.
+3. **Clique no botão "Descriptografar"** para decodificar o texto.
+4. **Clique no botão "Copiar"** para copiar o texto codificado ou decodificado para a área de transferência.
+
+
---
+++ interacao/script.js
@@ -0,0 +1,36 @@
+var textInput = document.querySelector("#input-texto");
+var outInput = document.querySelector("#output");
+
+function criptografar() {
+    var texto = textInput.value;
+
+    var result1 = texto.replace(/e/g, "enter");
+    var result2 = result1.replace(/i/g, "imes");
+    var result3 = result2.replace(/a/g, "ai");
+    var result4 = result3.replace(/o/g, "ober");
+    var result5 = result4.replace(/u/g, "ufat");
+    document.getElementById('output').innerHTML = '<textarea readonly id="output-texto">' + result5 + 
+    '</textarea>' + '<button class="btn-copiar" id="copiar" onclick="copiar()">Copiar</button>';
+}
+
+function descriptografar() {
+    var texto = textInput.value;
+
+    var result1 = texto.replace(/enter/g, "e");
+    var result2 = result1.replace(/imes/g, "i");
+    var result3 = result2.replace(/ai/g, "a");
+    var result4 = result3.replace(/ober/g, "o");
+    var result5 = result4.replace(/ufat/g, "u");
+
+    document.getElementById('output').innerHTML = '<textarea readonly id="output-texto">' + result5 + 
+    '</textarea>' + '<button class="btn-copiar" id="copiar" onclick="copiar()">Copiar</button>';
+}
+
+function copiar() {
+    var textoCop = document.getElementById('output-texto');
+    
+    textoCop.select();
+    document.execCommand('copy');
+    textInput.value = ''; // Limpa o conteúdo do textarea original
+    alert("Texto copiado para área de transferência.");
+}
